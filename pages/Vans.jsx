2391df9ee127afda9aa8c21ae96e12a949659248
NamaniNikhil <nikhilnaminini@gmail.com>
2024-02-23 23:40:44 -0500

    update vans.jsx file

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -21,16 +21,18 @@ export default function Vans() {
     }, [])
 
     const vanElements = vans.map(van => (
-        <Link to={`/vans/${van.id}`}>
+        
         <div key={van.id} className="van-tile">
+            <Link to={`/vans/${van.id}`}>
             <img src={van.imageUrl} />
             <div className="van-info">
                 <h3>{van.name}</h3>
                 <p>${van.price}<span>/day</span></p>
             </div>
             <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
-        </Link>
+       
     ))
 
     return (
