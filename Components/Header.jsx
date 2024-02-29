98ed56783fa219baa5af5ad827d1a2727389ff8a
NamaniNikhil <nikhilnaminini@gmail.com>
2024-02-29 01:47:06 -0500

    Update Header.jsx

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -2,7 +2,12 @@ import React from "react"
 import { Link , NavLink} from "react-router-dom"
 export default function Header(){
 
-
+    const activeLinks = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    
     
     
         return    (
@@ -10,10 +15,13 @@ export default function Header(){
                     <header>
                         <Link className="site-logo" to="/">#VanLife</Link>
                         <nav>
-                            <NavLink to="/about" className={({isActive}) => isActive ? "active-link" : null}>About
+                            <NavLink to="/about" style={({isActive}) => isActive ? 
+                            activeLinks : null}>About
                             </NavLink>
-                            <NavLink to="/vans" className={({isactive}) => isActive ? "active-link" : null}>Vans</NavLink>
-                            <NavLink to ="/host" className={({isactive}) => isActive ? "active-link" : null}>Host</NavLink>
+                            <NavLink to="/vans" style={({isActive}) => isActive ? 
+                            activeLinks : null}>Vans</NavLink>
+                            <NavLink to ="/host" style={({isActive}) => isActive ? 
+                            activeLinks : null}>Host</NavLink>
                         </nav>
                     </header>
                     
