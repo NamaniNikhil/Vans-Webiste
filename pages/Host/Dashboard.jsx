814040c31fe55dc6bda67b57709d957508d41fc4
NamaniNikhil <nikhilnaminini@gmail.com>
2024-02-28 23:01:34 -0500

    Outlet in Dashboard

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,6 +1,13 @@
 import React from "react"
 
+import { Outlet } from "react-router-dom"
 export default function Dashboard(){
 
-    return <h1>Host Dashboard is Here</h1>
+     
+    
+    return (<>
+        <h1>Host Dashboard is Here</h1>
+        <Outlet />
+    </>)
+   
 }
