388ea102281e917c8bd494c0acbba5e83543ca75
NamaniNikhil <nikhilnaminini@gmail.com>
2024-03-04 17:20:25 -0500

    Update HostLayout.jsx

--- Components/HostLayout.jsx
+++ Components/HostLayout.jsx
@@ -1,17 +1,21 @@
 import React from "react"
-import { Link , Outlet } from "react-router-dom"
+import { Link , NavLink, Outlet } from "react-router-dom"
 
 export default function HostLayout() {
 
-
+const activenavlinks = {
+    fontWeight : "bold",
+    textDecoration : "underline",
+    color : "#161616"
+}
 
     
 
     return  (<>
         <nav className="host-nav">
-            <Link to ="/host">Dashboard</Link>
-            <Link to = "/host/income">Income</Link>
-            <Link to = "/host/Reviews">Reviews</Link>
+            <NavLink   style={({isActive}) => isActive ? activenavlinks : null} to ="/host" end>Dashboard</NavLink>
+            <NavLink  style={({isActive}) => isActive ? activenavlinks : null}  to = "/host/income">Income</NavLink>
+            <NavLink   style={({isActive}) => isActive ? activenavlinks : null}  to = "/host/Reviews">Reviews</NavLink>
         </nav>
         <Outlet />
     </>
