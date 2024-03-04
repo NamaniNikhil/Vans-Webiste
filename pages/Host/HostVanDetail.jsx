46632485ebaad8170aabaef79845383625be92bf
NamaniNikhil <nikhilnaminini@gmail.com>
2024-03-04 18:20:15 -0500

    Update HostVanDetail.jsx

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -3,5 +3,32 @@ import React from "react"
 export default function HostVanDetail(){
 
 
-    return <h1>Host Van Details is Here</h1>
-}
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setCurrentVan(data.vans))
+    }, [])
+    
+    if (!currentVan) {
+        return <h1>Loading...</h1>
+    }
+
+    return (
+        <div>
+            <img src={currentVan.imageUrl} width={150} />
+            <h2>{currentVan.name}</h2>
+            <p>{currentVan.price}</p>
+            <p>{currentVan.type}</p>
+        </div>
+    )
+    
+    
+    
+    
+    
+    
+    
+} 
